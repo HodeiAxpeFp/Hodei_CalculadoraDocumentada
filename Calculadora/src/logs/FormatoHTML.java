ddde806ba0596b8136decb23bdfc5306e06ecfe9
irakaslea <irakaslea@DESKTOP-KFR68DH>
2022-03-23 11:14:30 +0100

    Formato cambiado a HTML

---
+++ Calculadora/src/logs/FormatoHTML.java
@@ -0,0 +1,25 @@
+package logs;
+
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+public class FormatoHTML extends Formatter {
+
+	@Override
+	public String format(LogRecord record) {
+		
+		return "<P>"+ record.getMessage()+ "\n</P>\n";
+	}
+	
+	@Override
+	public String getHead(Handler h) {
+        return "<HTML>\n <BODY> \n";
+    }
+	
+	@Override
+    public String getTail(Handler h) {
+        return "</BODY>\n </HTML> \n";
+    }
+
+}
--- Calculadora/src/principal/Calculadora.java
+++ Calculadora/src/principal/Calculadora.java
@@ -7,7 +7,9 @@ import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
+import java.util.logging.XMLFormatter;
 
+import logs.FormatoHTML;
 /**
  * @author Hodei Axpe
  */
@@ -24,34 +26,15 @@ public class Calculadora {
 		 * cuantas variables y una array de 2 posiciones El main contine tambien una
 		 * excepcion de tipo ArithmeticException
 		 */
-		
+		configurarLog();
+
 		int resultado = 0;
 		String operacion = "";
 		int[] operandos = new int[2];
 
 		Menu menu = new Menu();
 		Operaciones operaciones = new Operaciones();
-///////////////////////////////////////////////////////////////////////
-		Handler fileHandler = null;
-		Handler consoleHandler  = new ConsoleHandler();
-		try {
-		fileHandler  = new FileHandler("./operaciones.log");;
-		}catch(IOException exception){
-	        LOGGER.log(Level.SEVERE, "Ocurrio un error en FileHandler.", exception);
-	    }
-		LogManager.getLogManager().reset();
-		
-        //Asignar handlers al objeto LOGGER
-		 LOGGER.addHandler(consoleHandler);
-         LOGGER.addHandler(fileHandler);
-		
-         //Establecer niveles a handlers y LOGGER
-         consoleHandler.setLevel(Level.WARNING);
-         fileHandler.setLevel(Level.FINE);
-         LOGGER.setLevel(Level.FINE);
-         
-         
-        
+
 		do {
 			operandos = menu.pedirNumeros();
 			operacion = menu.menuOpciones();
@@ -74,12 +57,43 @@ public class Calculadora {
 				} else {
 					System.out.println("Operación no válida");
 				}
-				LOGGER.log(Level.FINE,"Operacion : "+ operacion + " | operando 1: " + operandos[0] + " | operando 2: " + operandos[1] + " | Resultado: " + resultado);
+				LOGGER.log(Level.FINE, "Operacion : " + operacion + " | operando 1: " + operandos[0] + " | operando 2: "
+						+ operandos[1] + " | Resultado: " + resultado);
 			} catch (ArithmeticException e) {
 				System.out.println("Operacion aritmetica no valida " + e.getMessage());
 				LOGGER.log(Level.WARNING, "Division entre cero.");
 			}
 
 		} while (menu.repetir());
+	}
+
+	public static void configurarLog() {
+	//	LogManager.getLogManager().reset();
+		
+		LOGGER.setUseParentHandlers(false);
+
+		Handler fileHandler = null;
+		Handler consoleHandler = new ConsoleHandler();
+		
+		// Asignar handlers al objeto LOGGER
+		LOGGER.addHandler(consoleHandler);
+		
+		try {
+			fileHandler = new FileHandler("./logOperaciones.html");
+		} catch (IOException exception) {
+			LOGGER.log(Level.SEVERE, "Ocurrio un error en FileHandler.", exception);
+		}
+
+		// Implementamos nuestro formateo de salida:
+		fileHandler.setFormatter(new FormatoHTML());
+		
+		// Asignar handlers al objeto LOGGER
+		LOGGER.addHandler(fileHandler);
+
+		// Establecer niveles a Handler y LOGGER
+		consoleHandler.setLevel(Level.WARNING);
+		fileHandler.setLevel(Level.FINE);
+
+		LOGGER.setLevel(Level.FINE);
 	}
 }
